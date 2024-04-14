b2ac4b87a2a0a48a7f181181646cb9eea5187e9c
T4nae <tan.b63000@gmail.com>
2024-04-14 19:39:57 +0530

    cors fix attempt

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,7 +12,7 @@ await db.connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 // app.use((req, res, next) => {
